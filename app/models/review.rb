865d9e3eb9ed71602c61a4757f6d6026862c950d
Marcos Costa <marcos.liborio@gmail.com>
2020-11-27 17:21:30 -0300

    Implementado e testado modelo

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,5 @@ class Review < ApplicationRecord
 
   validates :content, presence: true
   validates :rating, numericality: { only_integer: true }
-  # validates :content, presence: true
-  # validates :rating, numericality: { only_integer: true }
   validates :rating, inclusion: { in: (0..5) }
 end
